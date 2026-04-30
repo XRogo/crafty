8bc7f0a57fccb9560bcdf0051962288342e4085c
XRogo <gamesxrogo@gmail.com>
2026-04-30 21:52:40 +0200

    Aktualizacja mapy - zmiany zbiorcze

--- poligons/mackrater.js
+++ poligons/mackrater.js
@@ -1,30 +1,49 @@
-﻿window.registerPolygons([
+window.registerPolygons([
     {
-        number: 3,
-        points: [[1056, -190], [1143, -185], [1181, -119], [1163, -35], [1130, 13], [1085, 32], [981, -15], [985, -64], [1032, -80], [1038, -164]],
-        lineColor: '#1100faff',
-        fillColor: 'rgba(0, 4, 255, 0.2)',
+        points: [[38,-75],[45,-78],[134,-77],[136,-70],[136,-52],[135,-42],[125,-40],[110,-39],[90,-39],[86,-38],[83,-32],[80,-23],[78,-21],[72,-21],[70,-24],[67,-28],[66,-29],[63,-40],[61,-50],[53,-53],[46,-52],[42,-53],[39,-58]],
+        lineColor: "rgba(0, 132, 255, 1)",
+        fillColor: "rgba(0, 132, 255, 0.2)",
         closePath: true,
-        name: 'Farma kwiatów Maka',
-        category: 1,
+        name: "Neressia/maku?",
+        category: "terrain",
+        authors: ["MacKrater","Neressia"]
     },
     {
-        number: 4,
-        points: [[1100, -800], [1000, -900], [900, -800], [1000, -700]],
-        lineColor: '#1100faff',
-        fillColor: 'rgba(0, 4, 255, 0.2)',
+        points: [[130,-349],[167,-393],[221,-399],[296,-402],[346,-395],[383,-393],[395,-399],[403,-409],[408,-424],[401,-446],[366,-556],[315,-621],[220,-663],[176,-659],[123,-653],[70,-636],[-37,-592],[-74,-563],[-86,-536],[-80,-497],[-67,-443],[-50,-392],[-24,-359],[16,-331],[38,-323],[64,-322]],
+        lineColor: "rgba(7, 134, 213, 1)",
+        fillColor: "rgba(7, 134, 213, 0.2)",
         closePath: true,
-        name: 'Glina Maka',
-        category: 1,
+        name: "Baza Neressia/Maku",
+        category: "terrain",
+        authors: ["MacKrater","Neressia"]
     },
     {
-        points: [[-2245, 105], [-2245, 107], [-2245, 120], [-2214, 120], [-2214, 107], [-2214, 105]],
+        points: [[1056,-190],[1143,-185],[1181,-119],[1163,-35],[1130,13],[1085,32],[981,-15],[985,-64],[1032,-80],[1038,-164]],
+        lineColor: "#1100faff",
+        fillColor: "rgba(0, 4, 255, 0.2)",
+        closePath: true,
+        name: "Farma kwiatów Maka",
+        category: "terrain",
+        authors: ["MacKrater"]
+    },
+    {
+        points: [[1100,-800],[1000,-900],[900,-800],[1000,-700]],
+        lineColor: "#1100faff",
+        fillColor: "rgba(0, 4, 255, 0.2)",
+        closePath: true,
+        name: "Glina Maka",
+        category: "terrain",
+        authors: ["MacKrater"]
+    },
+    {
+        points: [[-2245,105],[-2245,107],[-2245,120],[-2214,120],[-2214,107],[-2214,105]],
         lineColor: "#ff4d00",
         fillColor: "#ff4d0033",
         closePath: true,
         name: "Stacja Halloween",
         category: "station",
-        in: [[-2214, 107]],
-        out: [[-2245, 107]],
+        in: [[-2214,107]],
+        out: [[-2245,107]],
+        authors: ["MacKrater"]
     },
 ]);
