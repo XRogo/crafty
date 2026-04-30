5f1bcdb548c1986dd753e964899cb62c68dcd55f
XRogo <gamesxrogo@gmail.com>
2026-04-30 15:29:11 +0200

    Aktualizacja mapy (Lottunia)

---
+++ poligons/lottunia.js
@@ -0,0 +1,11 @@
+window.registerPolygons([
+{
+ points: [[-34,-55],[-23,-58],[-21,-62],[-21,-65],[-25,-74],[-29,-78],[-30,-81],[-33,-83],[-39,-86],[-40,-89],[-44,-91],[-50,-90],[-51,-87],[-47,-76],[-45,-70],[-44,-65],[-42,-62]],
+ lineColor: "#a600ff",
+ fillColor: "#a600ff33",
+ closePath: true,
+ name: "Lottti",
+ category: "terrain",
+ authors: ["Lottunia"],
+},
+]);
