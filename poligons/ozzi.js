bbb1fa1ee510e36616b59e646c13f04bbf6186ad
XRogo <gamesxrogo@gmail.com>
2026-04-30 17:13:17 +0200

    Aktualizacja mapy - zmiany zbiorcze

--- poligons/ozzi.js
+++ poligons/ozzi.js
@@ -1,11 +1,20 @@
-﻿window.registerPolygons([
+window.registerPolygons([
     {
-        number: 12,
-        points: [[-9, 84], [-9, 72], [1, 62], [13, 54], [28, 57], [40, 63], [47, 65], [49, 72], [49, 84], [51, 90], [47, 100], [34, 110], [19, 114], [12, 106], [5, 94], [-5, 88]],
-        lineColor: 'rgba(107, 10, 49, 1)',
-        fillColor: 'rgba(107, 10, 49, 0.2)',
+        points: [[262,434],[158,433],[147,442],[130,447],[122,458],[100,463],[84,483],[84,503],[86,528],[103,564],[131,605],[168,625],[229,626],[262,604],[273,550],[284,453]],
+        lineColor: "rgba(44, 140, 242, 1)",
+        fillColor: "rgba(44, 140, 242, 0.2)",
         closePath: true,
-        name: 'OzzYczek',
-        category: 1,
+        name: "Podwodne miasto dasikora.",
+        category: "terrain",
+        authors: ["Dasikor","OzzI"]
+    },
+    {
+        points: [[-9,84],[-9,72],[1,62],[13,54],[28,57],[40,63],[47,65],[49,72],[49,84],[51,90],[47,100],[34,110],[19,114],[12,106],[5,94],[-5,88]],
+        lineColor: "rgba(107, 10, 49, 1)",
+        fillColor: "rgba(107, 10, 49, 0.2)",
+        closePath: true,
+        name: "OzzYczek",
+        category: "terrain",
+        authors: ["OzzI"]
     },
 ]);
