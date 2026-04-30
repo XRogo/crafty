3c00058de1352b74516ee892e22b4bbedad0fec8
XRogo <gamesxrogo@gmail.com>
2026-04-30 17:13:15 +0200

    Aktualizacja mapy - zmiany zbiorcze

--- poligons/dasikor.js
+++ poligons/dasikor.js
@@ -1,1 +1,11 @@
-﻿window.registerPolygons([]);
+window.registerPolygons([
+    {
+        points: [[262,434],[158,433],[147,442],[130,447],[122,458],[100,463],[84,483],[84,503],[86,528],[103,564],[131,605],[168,625],[229,626],[262,604],[273,550],[284,453]],
+        lineColor: "rgba(44, 140, 242, 1)",
+        fillColor: "rgba(44, 140, 242, 0.2)",
+        closePath: true,
+        name: "Podwodne miasto dasikora.",
+        category: "terrain",
+        authors: ["Dasikor","OzzI"]
+    },
+]);
