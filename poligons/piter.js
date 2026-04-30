44fec3a886149e75cc0e888a73cb1d7949e53bc7
XRogo <gamesxrogo@gmail.com>
2026-04-30 21:52:39 +0200

    Aktualizacja mapy - zmiany zbiorcze

--- poligons/piter.js
+++ poligons/piter.js
@@ -1,1 +1,11 @@
-﻿window.registerPolygons([]);
+window.registerPolygons([
+    {
+        points: [[51,-24],[47,-16],[48,-6],[50,3],[58,8],[68,8],[71,2],[72,-6],[82,-7],[82,-19],[72,-20],[63,-25]],
+        lineColor: "rgba(128, 0, 255, 1)",
+        fillColor: "rgba(128, 0, 255, 0.2)",
+        closePath: true,
+        name: "Ozzy Dasikor",
+        category: "terrain",
+        authors: ["Dasikor","OzzI","Piter"]
+    },
+]);
