1881a7a5fbeda759966110d29bd45b7bd37f96c1
XRogo <gamesxrogo@gmail.com>
2026-05-03 00:49:16 +0200

    Aktualizacja mapy - zmiany zbiorcze

---
+++ poligons/k_o_ozzy.js
@@ -0,0 +1,41 @@
+window.registerPolygons([
+    {
+        points: [[262,434],[149,432],[119,454],[95,465],[84,483],[84,503],[86,528],[103,564],[131,605],[168,625],[229,626],[262,604],[273,550],[284,453]],
+        lineColor: "rgba(44, 140, 242, 1)",
+        fillColor: "rgba(44, 140, 242, 0.2)",
+        closePath: true,
+        name: "Podwodne miasto dasikora.",
+        category: "terrain",
+        authors: ["Dasikor","K_O_Ozzy"]
+    },
+    {
+        points: [[51,-24],[47,-16],[48,-6],[50,3],[58,8],[68,8],[71,2],[72,-6],[82,-7],[82,-19],[72,-20],[63,-25]],
+        lineColor: "rgba(128, 0, 255, 1)",
+        fillColor: "rgba(128, 0, 255, 0.2)",
+        closePath: true,
+        name: "Ozzy Dasikor",
+        category: "terrain",
+        authors: ["Dasikor","K_O_Ozzy","PiterPanMis"]
+    },
+    {
+        points: [[25,56],[27,56],[27,57],[44,57],[44,56],[46,56],[46,45],[44,45],[44,44],[27,44],[27,45],[25,45]],
+        lineColor: "#df6501",
+        fillColor: "#df650133",
+        closePath: true,
+        name: "Stacja Ozzy",
+        category: "station",
+        authors: ["MorrisEk","K_O_Ozzy"],
+        nodes: [[20,50],[45,50]],
+        out: [[25,50],[46,50]],
+        author: "MorrisEk"
+    },
+    {
+        points: [[-9,84],[-9,72],[1,62],[13,54],[28,57],[40,63],[47,65],[49,72],[49,84],[51,90],[47,100],[34,110],[19,114],[12,106],[5,94],[-5,88]],
+        lineColor: "rgba(107, 10, 49, 1)",
+        fillColor: "rgba(107, 10, 49, 0.2)",
+        closePath: true,
+        name: "OzzYczek",
+        category: "terrain",
+        authors: ["K_O_Ozzy"]
+    },
+]);
