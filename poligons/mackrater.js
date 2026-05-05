2aef28ccaad7e1559811a29b6c183fcc509bab28
XRogo <gamesxrogo@gmail.com>
2026-05-05 18:51:58 +0200

    Aktualizacja mapy - zmiany zbiorcze

--- poligons/mackrater.js
+++ poligons/mackrater.js
@@ -1,6 +1,6 @@
-﻿window.registerPolygons([
+window.registerPolygons([
     {
-        points: [[1056, -190], [1143, -185], [1181, -119], [1163, -35], [1130, 13], [1085, 32], [981, -15], [985, -64], [1032, -80], [1038, -164]],
+        points: [[1056,-190],[1143,-185],[1181,-119],[1163,-35],[1130,13],[1085,32],[981,-15],[985,-64],[1032,-80],[1038,-164]],
         lineColor: "#1100faff",
         fillColor: "rgba(0, 4, 255, 0.2)",
         closePath: true,
@@ -9,7 +9,7 @@
         authors: ["MacKrater"]
     },
     {
-        points: [[1100, -800], [1000, -900], [900, -800], [1000, -700]],
+        points: [[1100,-800],[1000,-900],[900,-800],[1000,-700]],
         lineColor: "#1100faff",
         fillColor: "rgba(0, 4, 255, 0.2)",
         closePath: true,
@@ -18,13 +18,36 @@
         authors: ["MacKrater"]
     },
     {
-        points: [[-2245, 105], [-2245, 107], [-2245, 120], [-2214, 120], [-2214, 107], [-2214, 105]],
+        points: [[-2245,105],[-2245,107],[-2245,120],[-2214,120],[-2214,105]],
         lineColor: "#ff4d00",
         fillColor: "#ff4d0033",
         closePath: true,
         name: "Stacja Halloween",
         category: "station",
-        out: [[-2214, 107], [-2245, 107]],
-        authors: ["MacKrater"]
+        authors: ["MacKrater"],
+        out: [[-2214,107],[-2245,107]],
+        author: "MacKrater"
+    },
+    {
+        points: [[-40,-1281],[-20,-1281],[-20,-1263],[1,-1263],[1,-1255],[0,-1255],[0,-1242],[1,-1242],[1,-1235],[-20,-1235],[-20,-1217],[-40,-1217],[-40,-1241],[-39,-1241],[-39,-1257],[-40,-1257]],
+        lineColor: "#00eeff",
+        fillColor: "#00eeff33",
+        closePath: true,
+        name: "Stacja Miasteczko zimowe",
+        category: "station",
+        authors: ["MacKrater"],
+        out: [[-13,-1235],[-7,-1235],[-7,-1263],[-13,-1263]],
+        author: "MacKrater"
+    },
+    {
+        points: [[-618,-822],[-584,-822],[-585,-771],[-617,-770]],
+        lineColor: "#00ff00",
+        fillColor: "#00ff0033",
+        closePath: true,
+        name: "stacja modern miasto?",
+        category: "station",
+        authors: ["MacKrater"],
+        out: [[-601,-771],[-601,-822]],
+        author: "MacKrater"
     },
 ]);
