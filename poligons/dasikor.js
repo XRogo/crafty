f53a91f26076a522108095aca25c672850b8ded8
XRogo <gamesxrogo@gmail.com>
2026-05-28 22:00:26 +0200

    Aktualizacja mapy - zmiany zbiorcze

--- poligons/dasikor.js
+++ poligons/dasikor.js
@@ -1,3 +1,14 @@
 window.registerPolygons([
-
+    {
+        points: [[152,509],[151,528],[178,529],[179,509]],
+        lineColor: "#00ff00",
+        fillColor: "#00ff0033",
+        closePath: true,
+        name: "dasikor",
+        opis: "Dasikor nie mam pojecia gdzie bedzie stacja i wyjazd z niej ale to propozycja torów",
+        category: "station",
+        authors: ["Dasikor"],
+        author: "Dasikor",
+        out: [[179,517],[152,517]]
+    },
 ]);
